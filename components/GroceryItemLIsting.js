85d83971aadb267ae5be06a88fb410e265e5a6c6
Leon Si <leonzalion@gmail.com>
2020-03-27 20:05:38 -0400

    update

--- components/GroceryItemLIsting.js
+++ components/GroceryItemLIsting.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Card} from 'react-native-elements';
 
+function GroceryItemListing() {
+  const items = [
+    {
+
+    }
+  ];
+
+  return (
+
+  );
+}
+
+const styles = StyleSheet.create({
+  flex: 1,
+  flexWrap: 'wrap'
+});
