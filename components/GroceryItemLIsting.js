abdc656d9c171564e4ffc6bf2a90e97e15dc2170
Leon Si <leonzalion@gmail.com>
2020-03-27 20:17:08 -0400

    u

--- components/GroceryItemLIsting.js
+++ components/GroceryItemLIsting.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {Card} from 'react-native-elements';
 
-function GroceryItemListing() {
-  const items = [
-    {
-
-    }
-  ];
-
+function GroceryItemListing(props) {
   return (
-
+    <Card title={props.name}>
+      <Text>{props.price}</Text>
+    </Card>
   );
 }
 
 const styles = StyleSheet.create({
-  flex: 1,
-  flexWrap: 'wrap'
+
 });
