3943227e1d1a560b14ff4bbf391f0c9a5741fe79
AbstractUltra <dragonhe36@gmail.com>
2020-03-31 12:53:58 -0400

    Update stores to reflect actual items

--- screens/StoreSelectionScreen.js
+++ screens/StoreSelectionScreen.js
@@ -14,52 +14,12 @@ const storeLocations = [
     id: 0
   },
   {
-    name: "Walmart Erin Mills",
-    coords: {
-      latitude: 43.5603944,
-      longitude: -79.714509,
-    },
-    id: 1
-  },
-  {
-    name: "Walmart Markham",
-    coords: {
-      latitude: 43.867632,
-      longitude: -79.2929858,
-    },
-    id: 2
-  },
-  {
-    name: "Walmart Brampton East",
-    coords: {
-      latitude: 43.7407067,
-      longitude: -79.6972618,
-    },
-    id: 3
-  },
-  {
-    name: "Foody Mart Scarborough",
-    coords: {
-      latitude: 43.8151643,
-      longitude: -79.3248755,
-    },
-    id: 4
-  },
-  {
-    name: "Nations Fresh Foods",
-    coords: {
-      latitude: 43.5508372,
-      longitude: -79.7213554,
-    },
-    id: 5
-  },
-  {
     name: "Metro Warden Ave",
     coords: {
       latitude: 43.797456,
       longitude: -79.3200992,
     },
-    id: 6
+    id: 1
   },
 ];
 
