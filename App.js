7fbe0323f3a85ede990e32feba9f51601dda16cf
Leon Si <leonzalion@gmail.com>
2020-03-31 15:48:46 -0400

    made changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import GroceryItemSearchScreen from './screens/GroceryItemSearchScreen';
 import GroceryListSearchScreen from './screens/GroceryListSearchScreen';
@@ -16,6 +16,7 @@ import {
 } from "@react-navigation/drawer";
 import {Notifications} from 'expo';
 import * as Permissions from 'expo-permissions';
+import * as SecureStore from 'expo-secure-store';
 
 const Drawer = createDrawerNavigator();
 
@@ -48,6 +49,21 @@ export default function App() {
   const [currentStore, setCurrentStore] = useState({name: "Walmart", id: 0});
   const [groceryList, setGroceryList] = useState({});
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    (async() => {
+      const pushToken = await SecureStore.getItemAsync('pushToken');
+      if (pushToken) return;
+
+      const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (status !== 'granted') {
+        alert('No notification permissions!');
+        return;
+      }
+      let token = await Notifications.getExpoPushTokenAsync();
+      await SecureStore.setItemAsync('pushToken', token);
+    })();
+  }, []);
 
   React.useEffect(() => {
     isMountedRef.current = true;
