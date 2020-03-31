e9ef8ea8f86bacb59fb29473b5565ef2338b8cf6
Leon Si <leonzalion@gmail.com>
2020-03-31 13:55:34 -0400

    made changes

--- components/GroceryItemListing.js
+++ components/GroceryItemListing.js
@@ -1,53 +1,50 @@
 import React, {useContext} from 'react';
-import {Dimensions, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {Text, Dimensions, StyleSheet, TouchableOpacity} from 'react-native';
 import {Button, Card, Rating} from 'react-native-elements';
 import GroceryListContext from "../contexts/GroceryList";
 
+
 export default React.memo(function GroceryItemListing({
-	_id,
-	name,
-	imageUrl,
-	rating,
-	price
+	_id, name, imageUrl, rating, price
 }) {
   const {groceryList, setGroceryList} = useContext(GroceryListContext);
 
 	return (
-			<Card
-				title={name}
-        titleStyle={{flexWrap: 'wrap'}}
-				containerStyle={styles.cardContainer}
-        wrapperStyle={styles.cardWrapper}
-				imageProps={{resizeMode: 'contain'}}
-				image={{uri: imageUrl}}
-			>
-				<Rating
-					imageSize={20}
-					readonly
-					startingValue={rating}
-          style={{margin: 10}}
-				/>
-				<Button
-					title={`\$${price}`}
-          titleStyle={styles.price}
-					onPress={() => {
-						groceryList[_id] = groceryList[_id] || {
-							name,
-							imageUrl,
-							price,
-							count: 0,
+		<Card
+			title={<Text style={{fontSize: 16}}>{name}</Text>}
+			titleStyle={{flexWrap: 'wrap'}}
+			containerStyle={styles.cardContainer}
+			wrapperStyle={styles.cardWrapper}
+			imageProps={{resizeMode: 'contain'}}
+			image={{uri: imageUrl}}
+		>
+			<Rating
+				imageSize={20}
+				readonly
+				startingValue={rating}
+				style={{margin: 10}}
+			/>
+			<Button
+				title={`\$${price}`}
+				titleStyle={styles.price}
+				onPress={() => {
+					groceryList[_id] = groceryList[_id] || {
+						name,
+						imageUrl,
+						price,
+						count: 0,
+					};
+					const count = groceryList[_id].count;
+					if (count < 9) {
+						const newItem = {
+							...groceryList[_id],
+							count: count + 1
 						};
-						const count = groceryList[_id].count;
-						if (count < 9) {
-							const newItem = {
-								...groceryList[_id],
-								count: count + 1
-							};
-							setGroceryList({...groceryList, [_id]: newItem});
-						}
-					}}
-				/>
-			</Card>
+						setGroceryList({...groceryList, [_id]: newItem});
+					}
+				}}
+			/>
+		</Card>
 	)
 });
 
