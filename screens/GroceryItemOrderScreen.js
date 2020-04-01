08616647a19e2b34fae54dd9e86653c9d5fe69e8
AbstractUltra <dragonhe36@gmail.com>
2020-04-01 14:27:03 -0400

    Order screen

--- screens/GroceryItemOrderScreen.js
+++ screens/GroceryItemOrderScreen.js
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
-import {Button} from "react-native-elements";
+import {Text, StyleSheet, View} from 'react-native';
+import {Button, Image, Rating} from "react-native-elements";
 import GroceryListContext from "../contexts/GroceryList";
 
 export default function GroceryItemScreen({navigation, route}) {
@@ -8,54 +8,70 @@ export default function GroceryItemScreen({navigation, route}) {
   const [itemQuantity, setItemQuantity] = useState(1);
   return (
     <View style={styles.pageStyle}>
-      <Image source={{uri: route.params.imageUrl}} style={styles.imageStyle}/>
-      <View style={styles.counterStyle}>
-        <Button
-          icon={{name: 'minus', type: 'entypo'}}
-          type='clear'
-          onPress={() => {
-            if (itemQuantity > 1) setItemQuantity(itemQuantity-1);
-          }}
+      <View style={styles.descContainer}>
+        <Text style={{fontWeight:"bold", fontSize: 20, padding:20}}>{route.params.name}</Text>
+        <Image source={{uri: route.params.imageUrl}} style={styles.imageStyle}/>
+        <Rating
+          imageSize={20}
+          readonly
+          startingValue={route.params.rating}
         />
-        <Text style={styles.count}>{itemQuantity}</Text>
+        <Text style={{padding: 10, fontSize: 15, fontWeight: "bold"}}>${route.params.price.toFixed(2)}</Text>
+        <View style={styles.counterStyle}>
+          <Button
+            icon={{name: 'minus', type: 'entypo'}}
+            type='clear'
+            onPress={() => {
+              if (itemQuantity > 1) setItemQuantity(itemQuantity-1);
+            }}
+          />
+          <Text style={styles.count}>{itemQuantity}</Text>
+          <Button
+            type="clear"
+            icon={{name: 'plus', type: 'entypo'}}
+            onPress={() => {
+              if (itemQuantity < 9) setItemQuantity(itemQuantity+1);
+            }}
+          />
+        </View>
+      </View>
+
+      <View style={styles.buttonContainer}>
         <Button
-          type="clear"
-          icon={{name: 'plus', type: 'entypo'}}
+          title={"Add to Cart"}
+          titleStyle={styles.buttonTextStyle}
+          buttonStyle={{height:50}}
           onPress={() => {
-            if (itemQuantity < 9) setItemQuantity(itemQuantity+1);
+            groceryList[route.params._id] = groceryList[route.params._id] || {
+              name: route.params.name,
+              imageUrl: route.params.imageUrl,
+              price: route.params.price,
+              count: 0,
+            };
+            const count = groceryList[route.params._id].count;
+            if (count < 9) {
+              const newItem = {
+                ...groceryList[route.params._id],
+                count: count + 1
+              };
+              setGroceryList({...groceryList, [route.params._id]: newItem});
+            }
+            navigation.pop();
           }}
         />
       </View>
-      <Button
-        title={"Add to Cart"}
-        titleStyle={styles.buttonTextStyle}
-        style={styles.buttonStyle}
-        onPress={() => {
-          groceryList[route.params._id] = groceryList[route.params._id] || {
-            name: route.params.name,
-            imageUrl: route.params.imageUrl,
-            price: route.params.price,
-            count: 0,
-          };
-          const count = groceryList[route.params._id].count;
-          if (count < 9) {
-            const newItem = {
-              ...groceryList[route.params._id],
-              count: count + 1
-            };
-            setGroceryList({...groceryList, [route.params._id]: newItem});
-          }
-          navigation.pop();
-        }}
-      />
     </View>
   )
 }
 const styles = StyleSheet.create({
   pageStyle: {
     flex: 1,
+    backgroundColor: "#ffffff",
   },
-  buttonStyle: {
+  descContainer: {
+    alignItems: "center"
+  },
+  buttonContainer: {
     flex: 1,
     justifyContent: "flex-end"
   },
@@ -68,7 +84,7 @@ const styles = StyleSheet.create({
   },
   imageStyle: {
     width:200,
-    height:200
+    height:200,
   },
   count: {
     alignSelf: 'center',
